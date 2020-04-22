2c7c54f8ddba69c774c4acea3c9ebc17056a6175
maxclayton <maxclayton07@gmail.com>
2020-04-22 08:07:43 -0600

    done

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+async function getHotelData() {
+    try {
+        const response = await fetch('js/hotel.json')
+        return await response.json() // Return the JSON object
+    } catch (error) {
+        console.error()
+    }
+}
+
+let hotelData = {}
+getHotelData().then(data => hotelData = data)
+
+document.querySelector("#marriott").addEventListener('click', hotelInfo)
+document.querySelector("#sharaton").addEventListener('click', hotelInfo)
+document.querySelector("#hilton").addEventListener('click', hotelInfo)
+
+function hotelInfo(event) {
+    let hotelChoice = hotelData.hotels.find(hotel => {
+        return event.target.id === hotel.name.toLowerCase()
+    })
+    console.log(hotelChoice)
+
+    document.querySelector("#hotelName").textContent = `${hotelChoice.name} Hotel`
+
+    document.querySelector("#address").textContent = `${hotelChoice.address}`
+
+    document.querySelector("#rooms").textContent = `${hotelChoice.rooms}`
+
+    document.querySelector("#gym").textContent = `${hotelChoice.gym}`
+
+    document.querySelector("#type").textContent = `${hotelChoice.roomTypes}`
+
+    let imagePath = `${hotelChoice.picture}`
+
+    document.querySelector("#picture").setAttribute('src', imagePath)
+
+}
